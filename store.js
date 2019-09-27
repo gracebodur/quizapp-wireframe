9f7a5da3e02a0304693c3ce635d3db6ed038afc2
Gracie <50187664+gracebodur@users.noreply.github.com>
2019-09-26 21:30:03 -0400

    Add files via upload

---
+++ store.js
@@ -0,0 +1,106 @@
+const STORE = {
+    questions: [//1
+      {
+        question: "",
+        options: [
+          "", 
+          "", 
+          "", 
+          ""
+        ],
+        answer: ""
+      },
+      //2
+      {
+        question: "",
+        options: [
+          "",
+          "", 
+          "",
+          ""
+        ],
+        answer: ""
+      },
+      //3
+      {
+        question: "",
+        options: [
+          "", 
+          "", 
+          "", 
+          ""
+        ],
+        answer: ""
+      },
+      //4
+      {
+        question: "", 
+        options: [
+            "",
+             "", 
+            "",
+            ""],
+        answer: ""
+      },
+      //5
+      {
+        question: "",
+        options: [
+          "", 
+          "", 
+          "", 
+          ""
+        ],
+        answer: ""
+      },
+      //6
+      {
+        question: "",
+        options: [
+          "", 
+          "", 
+          "", 
+          ""
+        ],
+        answer: " "
+      },
+      //7
+      {
+        question: " ",
+        options: [
+          " ", 
+          "", 
+          " ", 
+          " "
+        ],
+        answer: ""
+      },
+      //8
+      {
+        question: "", 
+        options: [
+          "", 
+          "", 
+          "", 
+          ""
+        ],
+        answer: ""
+      },
+      //9
+      {
+        question: "",
+        options: [
+          "", 
+          "", 
+          "", 
+          ""
+        ], 
+        answer: ""
+      },
+      //10
+      {
+      }
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
